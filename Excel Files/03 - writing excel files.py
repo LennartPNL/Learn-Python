d82ff7d49f311c9a74e0b22605d1f1078521d350
Piraato <lp.lennartpikijn@gmail.com>
2018-06-06 09:13:02 +0200

    Writing excel files

---
+++ Excel Files/03 - writing excel files.py
@@ -0,0 +1,49 @@
+import openpyxl
+
+wb = openpyxl.Workbook()
+
+print(wb.sheetnames)
+
+sheet = wb.active
+
+print(sheet.title)
+
+
+# Changing the name of the sheet
+sheet.title = 'I just took a sheet'
+
+print(sheet.title)
+
+# Saving the workbook to a file
+wb.save('changedSheet.xlsx')
+
+### New workbook ###
+
+workbook = openpyxl.Workbook()
+
+# Adds a new sheet to the file
+workbook.create_sheet()
+
+print(workbook.sheetnames)
+
+workbook.create_sheet(index=0, title='First Sheet')
+
+workbook.create_sheet(index=2, title='Cool Sheet')
+
+print(workbook.sheetnames)
+
+# Removing sheets
+
+workbook.remove(workbook['Sheet1'])
+
+print(workbook.sheetnames)
+
+# Writing values
+
+worksheet = workbook['First Sheet']
+
+worksheet['A1'] = 'Title of column A'
+
+print(worksheet['A1'].value)
+
+workbook.save('writecells.xlsx')
